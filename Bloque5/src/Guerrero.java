67a4b7888abef43d0cad088f2d90dedd260e6d21
Gabriel Calle Home <gabriel.calle92@gmail.com>
2015-09-24 01:38:43 +0200

    commiit

--- Bloque5/src/Guerrero.java
+++ Bloque5/src/Guerrero.java
@@ -3,4 +3,22 @@ public class Guerrero {
 	String nombre;
 	int fuerza;
 	int salud;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getFuerza() {
+		return fuerza;
+	}
+	public void setFuerza(int fuerza) {
+		this.fuerza = fuerza;
+	}
+	public int getSalud() {
+		return salud;
+	}
+	public void setSalud(int salud) {
+		this.salud = salud;
+	}
 }
