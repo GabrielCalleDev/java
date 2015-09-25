ce29ae51476f127b8e816fc87e852de0c742d030
Gabriel Calle Torrez Indra <gabriel.calle92@gmail.com>
2015-09-25 07:06:23 +0200

    Commit

--- Bloque6/src/Bloque63ConstructoresAmpliados.java
+++ Bloque6/src/Bloque63ConstructoresAmpliados.java
@@ -1,3 +1,5 @@
+// Bloque Java 6.3: Ampliando Constructores
+// https://www.youtube.com/watch?v=XMKsCECAFOI&index=31&list=PLw8RQJQ8K1yQDqPyDRzt-h8YlBj96OwMP
 
 public class Bloque63ConstructoresAmpliados {
 
